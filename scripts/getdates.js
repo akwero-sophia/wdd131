4586dedf19e47706c978a5f4f0ff342719538c3c
myself <akwerosophia@gmail.com>
2025-03-02 22:33:25 +0300

    weee

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+<script src="scripts/getdates.js" defer></script>
+// Get the current year
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
+
+// Get the last modified date
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
